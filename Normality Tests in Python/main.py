13df315e2ea9e34a1627cfaa00c18657d3b504db
Abdelrahman Elsayed <abdoelsayed2016@gmail.com>
2020-11-01 16:19:35 +0600

    test

---
+++ Normality Tests in Python/main.py
@@ -0,0 +1,14 @@
+"""Normality Assumption
+
+A large fraction of the field of statistics is concerned with data that assumes that it was drawn from a Gaussian distribution.
+
+If methods are used that assume a Gaussian distribution, and your data was drawn from a different distribution, the findings may be misleading or plain wrong.
+
+There are a number of techniques that you can check if your data sample is Gaussian or sufficiently Gaussian-like to use the standard techniques, or sufficiently non-Gaussian to instead use non-parametric statistical methods.
+
+This is a key decision point when it comes to choosing statistical methods for your data sample. We can summarize this decision as follows:
+
+
+
+
+"""
