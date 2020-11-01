a05448b9bded0971ba78c5cfaeb4ac53925fcb59
Abdelrahman Elsayed <abdoelsayed2016@gmail.com>
2020-11-01 16:21:34 +0600

    test

--- Normality Tests in Python/main.py
+++ Normality Tests in Python/main.py
@@ -8,7 +8,50 @@ There are a number of techniques that you can check if your data sample is Gauss
 
 This is a key decision point when it comes to choosing statistical methods for your data sample. We can summarize this decision as follows:
 
+If Data Is Gaussian:
+	Use Parametric Statistical Methods
+Else:
+	Use Nonparametric Statistical Methods
 
+There is also some middle ground where we can assume that the data is Gaussian-enough to use parametric methods or that we can use data preparation techniques to transform the data to be sufficiently Gaussian to use the parametric methods.
 
+There are three main areas where you may need to make this evaluation of a data sample in a machine learning project; they are:
 
+Input data to the model in the case of fitting models.
+Model evaluation results in the case of model selection.
+Residual errors from model predictions in the case of regression.
+In this tutorial, we will look at two classes of techniques for checking whether a sample of data is Gaussian:
+
+Graphical Methods. These are methods for plotting the data and qualitatively evaluating whether the data looks Gaussian.
+Statistical Tests. These are methods that calculate statistics on the data and quantify how likely it is that the data was drawn from a Gaussian distribution.
+Methods of this type are often called normality tests.
+
+Test Dataset
+Before we start looking at normality tests, let’s first develop a test dataset that we can use throughout this tutorial.
+
+We will generate a small sample of random numbers drawn from a Gaussian distribution.
+
+The choice of Gaussian random numbers for the test dataset means that we do expect each test to correctly identify the distribution, nevertheless, the small-ish sample size may introduce some noise into the results.
+
+We will use the randn() NumPy function to generate random Gaussian numbers with a mean of 0 and a standard deviation of 1, so-called standard, normal variables. We will then shift them to have a mean of 50 and a standard deviation of 5.
+
+The complete example is listed below.
+"""
+# generate gaussian data
+from numpy.random import seed
+from numpy.random import randn
+from numpy import mean
+from numpy import std
+# seed the random number generator
+seed(1)
+# generate univariate observations
+data = 5 * randn(100) + 50
+# summarize
+print('mean=%.3f stdv=%.3f' % (mean(data), std(data)))
+""""
+Running the example generates the sample and prints the mean and standard deviation of the sample.
+
+We can see that the mean and standard deviation are reasonable but rough estimations of the true underlying population mean and standard deviation, given the small-ish sample size.
+
+mean=50.303 stdv=4.426
 """
