4e7e1f87aeab8e9f07ee6712d567b4f72ca432ef
Abdelrahman Elsayed <abdoelsayed2016@gmail.com>
2020-11-01 16:23:18 +0600

    test

--- Normality Tests in Python/main.py
+++ Normality Tests in Python/main.py
@@ -54,4 +54,83 @@ Running the example generates the sample and prints the mean and standard deviat
 We can see that the mean and standard deviation are reasonable but rough estimations of the true underlying population mean and standard deviation, given the small-ish sample size.
 
 mean=50.303 stdv=4.426
+
+Visual Normality Checks
+We can create plots of the data to check whether it is Gaussian.
+
+These checks are qualitative, so less accurate than the statistical methods we will calculate in the next section. Nevertheless, they are fast and like the statistical tests, must still be interpreted before you can make a call about your data sample.
+
+In this section, we will look at two common methods for visually inspecting a dataset to check if it was drawn from a Gaussian distribution.
 """
+
+
+
+"""
+
+Histogram Plot
+A simple and commonly used plot to quickly check the distribution of a sample of data is the histogram.
+
+In the histogram, the data is divided into a pre-specified number of groups called bins. The data is then sorted into each bin and the count of the number of observations in each bin is retained.
+
+The plot shows the bins across the x-axis maintaining their ordinal relationship, and the count in each bin on the y-axis.
+
+A sample of data has a Gaussian distribution of the histogram plot, showing the familiar bell shape.
+
+A histogram can be created using the hist() matplotlib function. By default, the number of bins is automatically estimated from the data sample.
+
+A complete example demonstrating the histogram plot on the test problem is listed below.
+"""
+
+
+# histogram plot
+from numpy.random import seed
+from numpy.random import randn
+from matplotlib import pyplot
+# seed the random number generator
+seed(1)
+# generate univariate observations
+data = 5 * randn(100) + 50
+# histogram plot
+pyplot.hist(data)
+pyplot.show()
+"""
+Running the example creates a histogram plot showing the number of observations in each bin.
+
+We can see a Gaussian-like shape to the data, that although is not strongly the familiar bell-shape, is a rough approximation.
+"""
+
+
+"""
+Quantile-Quantile Plot
+Another popular plot for checking the distribution of a data sample is the quantile-quantile plot, Q-Q plot, or QQ plot for short.
+
+This plot generates its own sample of the idealized distribution that we are comparing with, in this case the Gaussian distribution. The idealized samples are divided into groups (e.g. 5), called quantiles. Each data point in the sample is paired with a similar member from the idealized distribution at the same cumulative distribution.
+
+The resulting points are plotted as a scatter plot with the idealized value on the x-axis and the data sample on the y-axis.
+
+A perfect match for the distribution will be shown by a line of dots on a 45-degree angle from the bottom left of the plot to the top right. Often a line is drawn on the plot to help make this expectation clear. Deviations by the dots from the line shows a deviation from the expected distribution.
+
+We can develop a QQ plot in Python using the qqplot() statsmodels function. The function takes the data sample and by default assumes we are comparing it to a Gaussian distribution. We can draw the standardized line by setting the ‘line‘ argument to ‘s‘.
+
+A complete example of plotting the test dataset as a QQ plot is provided below.
+
+
+Running the example creates the QQ plot showing the scatter plot of points in a diagonal line, closely fitting the expected diagonal pattern for a sample from a Gaussian distribution.
+
+There are a few small deviations, especially at the bottom of the plot, which is to be expected given the small data sample.
+"""
+
+
+
+# QQ Plot
+from numpy.random import seed
+from numpy.random import randn
+from statsmodels.graphics.gofplots import qqplot
+from matplotlib import pyplot
+# seed the random number generator
+seed(1)
+# generate univariate observations
+data = 5 * randn(100) + 50
+# q-q plot
+qqplot(data, line='s')
+pyplot.show()
